22326cae196417283891a5203aeeb90c4472a3da
prakriti <prakriti673@gmail.com>
2023-10-21 16:49:36 +0530

    gefe

--- jugaad_junc/build.sh
+++ jugaad_junc/build.sh
@@ -1,10 +1,10 @@
 echo "Building the project..."
-python3.11.5 -m pip install -r requirements.txt
+python3.11 -m pip install -r requirements.txt
 
 echo "Make migrations.."
-python3.11.5 manage.py makemigrations --noinput
-python3.11.5 manage.py migrate --noinput
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
 
 echo "Collect static...."
-python3.11.5 manage.py collectstatic --noinput --clear
+python3.11 manage.py collectstatic --noinput --clear
 
