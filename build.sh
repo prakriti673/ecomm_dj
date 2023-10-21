21ca0c9a02ada96c711c79f0aa54dd7de0eaa505
prakriti <prakriti673@gmail.com>
2023-10-21 15:46:38 +0530

    try

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
 
-#Build the project
-echo "Building the project..."
-python3.11.5 -m pip install -r requirements.txt
+# echo "Building the project..."
+# python3.11.5 -m pip install -r requirements.txt
 
-echo "Make migrations.."
-python3.11.5 manage.py makemigrations --noinput
-python3.11.5 manage.py migrate --noinput
+# echo "Make migrations.."
+# python3.11.5 manage.py makemigrations --noinput
+# python3.11.5 manage.py migrate --noinput
 
-echo "Collect static...."
-python3.11.5 manage.py collectstatic --noinput --clear
+# echo "Collect static...."
+# python3.11.5 manage.py collectstatic --noinput --clear
 
