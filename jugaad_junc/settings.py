9136fe8a3c0be5b00fff0204eb55e82761cbbd9e
prakriti <prakriti673@gmail.com>
2023-10-21 15:57:08 +0530

    try3

--- jugaad_junc/settings.py
+++ jugaad_junc/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/ "staticfiles_build" / "static"
+STATIC_ROOT = BASE_DIR / "static"
 # MEDIA_URL = 'media/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
